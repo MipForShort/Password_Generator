869f14f128342e855c445993b65a7e2c78255e64
Mario Mip <mariomip98@gmail.com>
2025-01-07 10:36:14 -0600

    Finished main and moved generate_password below, I think it looks better

--- pass_gen.c
+++ pass_gen.c
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
+void generate_password(int length);
+
+int main ()
+{
+    /* We set the password_length and then we ask for it to scan it */
+    int password_length;
+
+    printf("Introduce the password length:\n");
+    scanf("%d", &password_length);
+
+    /* If the password is less than 1, then we say we don't do that
+    *in this place and we exit
+    */
+    if (password_length <= 0)
+    {
+        printf("The password length should be greater than 0\n");
+        return 1;
+    }
+
+    /* We call what is truly the main funct */
+    generate_password(password_length);
+
+    return 0;
+}
+
 void generate_password(int length)
 {
     /* We set the char array that includes all letters, digits & spec. symbols */
@@ -11,16 +36,16 @@ void generate_password(int length)
                 "!@#$%^&*()-_+";
 
     /* We set an integer sizeof our char array - 1
-        so it excludes the '\0' at the end of the string charset
+    *so it excludes the '\0' at the end of the string charset
     */
     int charset_size = sizeof(charset) - 1;
 
     char password[length + 1]; /* +1 to add '\0' at the end */
 
     /* We do a for loop with the length and we use arc4random_uniform,
-        that function gives us a better quality random number from
-        the integer that we set before and adds it to index.
-        Next we set the password[i] to the index that we got for charset
+    *that function gives us a better quality random number from
+    *the integer that we set before and adds it to index.
+    *Next we set the password[i] to the index that we got for charset
     */
     for (int i = 0; i < length; i++)
     {
